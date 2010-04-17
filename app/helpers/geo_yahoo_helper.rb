dbb3ddb7271cdb82ef704d99f5ef733a3487857b
Wadomo <all@wadomo.com>
2010-04-17 16:23:19 +0200

    [MINOR] Minor fix

--- app/helpers/geo_yahoo_helper.rb
+++ app/helpers/geo_yahoo_helper.rb
@@ -1,7 +1,7 @@
 module GeoYahooHelper
  def geo_autocomplete(id,updates)
     js ="jQuery.noConflict();\n"
-    js ="$('##{id}').autocomplete('/#{url}');\n"
+    js ="$('##{id}').autocomplete('/geoyahoo/cities');\n"
     js += "$('##{id}').result(function(event, data, formatted) {\n"
     js += "aux=jQuery(data);\n"
     updates.each_with_index {|item, index|
